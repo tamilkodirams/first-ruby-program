36c6ec51fff00cfc5db09920ffb8de7c5399c668
tamil <hod.mca@giet.ac.in>
2021-09-08 18:28:42 +0530

    My first ruby program for git

---
+++ test.rb
@@ -0,0 +1,16 @@
+# Write a comment
+# puts and gets
+# /n/n
+# string interpreattion
+# variables
+
+puts " abcd efgh #(1+10)"
+
+puts " what is your next todo?"
+next_todo = gets
+
+# in the middle of the program
+
+"puts /n/n/n"
+
+puts = "[] #{next_todo} #{next_todo}"
